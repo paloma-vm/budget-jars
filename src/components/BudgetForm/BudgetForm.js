df3a18d37820708f05e845b0eedc34e7eb0df159
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-03-14 23:28:09 -0600

    added fixed expenses

--- src/components/BudgetForm/BudgetForm.js
+++ src/components/BudgetForm/BudgetForm.js
@@ -43,88 +43,198 @@ function BudgetForm() {
   const [familyGifts, setFamilyGifts] = useState('')
   const [vacation, setVacation] = useState('')
   const [hobby, setHobby] = useState('')
+  const [sports, setSports] = useState('')
+  const [clubUnion, setClubUnion] = useState('')
+  const [charity, setCharity] = useState('')
+  const [bankFees, setBankFees] = useState('')
+  const [taxes, setTaxes] = useState('')
+  const [debtRepayment, setDebtRepayment] = useState('')
+  const [emergencyFund, setEmergencyFund] = useState('')
+  const [educationSavings, setEducationSavings] = useState('')
+  const [retirementSavings, setRetirementSavings] = useState('')
 
 
   return (
     <div className='BudgetForm'>
+      <h2>
+        Please enter monthly amounts in the blank spaces. 
+        If an item does not apply, please leave the space blank.
+      </h2>
+
       <h3>Total Income</h3>
       <form className='TotalIncome'>
-        <label>Net income #1</label>
-        <input
-          type="number"
-          value={netIncome1} 
-          onChange={(e) => setNetIncome1(e.target.value)}
-        />
-        <label>Net income #2</label>
-        <input
-          type="number"
-          value={netIncome2} 
-          onChange={(e) => setNetIncome2(e.target.value)}
-        />
-        <label>Rental income</label>
-        <input
-          type="number"
-          value={rentalIncome} 
-          onChange={(e) => setRentalIncome(e.target.value)}
-        />
-        <label>Spousal/Child Support</label>
-        <input
-          type="number"
-          value={spousalChild} 
-          onChange={(e) => setSpousalChild(e.target.value)}
-        />
-        <label>Government Benefits</label>
-        <input
-          type="number"
-          value={govtBenefits} 
-          onChange={(e) => setGovtBenefits(e.target.value)}
-        />
-        <label>Other income</label>
-        <input
-          type="number"
-          value={otherIncome} 
-          onChange={(e) => setOtherIncome(e.target.value)}
-        />
+        <div className='row'>
+          <label>Net income #1</label>
+          <input
+            type="number"
+            value={netIncome1}
+            onChange={(e) => setNetIncome1(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Net income #2</label>
+          <input
+            type="number"
+            value={netIncome2}
+            onChange={(e) => setNetIncome2(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Rental income</label>
+          <input
+            type="number"
+            value={rentalIncome}
+            onChange={(e) => setRentalIncome(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Spousal/Child Support</label>
+          <input
+            type="number"
+            value={spousalChild}
+            onChange={(e) => setSpousalChild(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Government Benefits</label>
+          <input
+            type="number"
+            value={govtBenefits}
+            onChange={(e) => setGovtBenefits(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Other income</label>
+          <input
+            type="number"
+            value={otherIncome}
+            onChange={(e) => setOtherIncome(e.target.value)}
+          />
+        </div>
       </form>
 
       <h3>Fixed Expenses</h3>
       <form className='FixedExpenses'>
-        <label>Mortgage OR Rent</label>
-        <input
-          type="number"
-          value={netIncome1} 
-          onChange={(e) => setNetIncome1(e.target.value)}
-        />
-        <label>Net income #2</label>
-        <input
-          type="number"
-          value={netIncome2} 
-          onChange={(e) => setNetIncome2(e.target.value)}
-        />
-        <label>Rental income</label>
-        <input
-          type="number"
-          value={rentalIncome} 
-          onChange={(e) => setRentalIncome(e.target.value)}
-        />
-        <label>Spousal/Child Support</label>
-        <input
-          type="number"
-          value={spousalChild} 
-          onChange={(e) => setSpousalChild(e.target.value)}
-        />
-        <label>Government Benefits</label>
-        <input
-          type="number"
-          value={govtBenefits} 
-          onChange={(e) => setGovtBenefits(e.target.value)}
-        />
-        <label>Other income</label>
-        <input
-          type="number"
-          value={otherIncome} 
-          onChange={(e) => setOtherIncome(e.target.value)}
-        />
+        <div className='row'>
+          <label>Mortgage or Rent</label>
+          <input
+            type="number"
+            value={mortgage}
+            onChange={(e) => setMortgage(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Property Tax</label>
+          <input
+            type="number"
+            value={propertyTax}
+            onChange={(e) => setpropertyTax(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Electricity</label>
+          <input
+            type="number"
+            value={electricity}
+            onChange={(e) => setElectricity(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Gas and Water</label>
+          <input
+            type="number"
+            value={gasWater}
+            onChange={(e) => setGasWater(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Maintenance Fees</label>
+          <input
+            type="number"
+            value={maintenanceFees}
+            onChange={(e) => setMaintenanceFees(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Cable</label>
+          <input
+            type="number"
+            value={cable}
+            onChange={(e) => setCable(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Phone #1</label>
+          <input
+            type="number"
+            value={phone1}
+            onChange={(e) => setPhone1(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Phone #2</label>
+          <input
+            type="number"
+            value={phone2}
+            onChange={(e) => setPhone2(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Internet</label>
+          <input
+            type="number"
+            value={internet}
+            onChange={(e) => setInternet(e.target.value)}
+          />    
+        </div>
+        <div className='row'>
+          <label>Home Insurance</label>
+          <input
+            type="number"
+            value={homeInsurance}
+            onChange={(e) => setHomeInsurance(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Childcare</label>
+          <input
+            type="number"
+            value={childcare}
+            onChange={(e) => setChildcare(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Car #1</label>
+          <input
+            type="number"
+            value={car1}
+            onChange={(e) => setCar1(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Car #2</label>
+          <input
+            type="number"
+            value={car2}
+            onChange={(e) => setCar2(e.target.value)}
+          />
+        </div>
+        <div className='row'>
+          <label>Car insurance and registration</label>
+          <input
+            type="number"
+            value={carInsRegn}
+            onChange={(e) => setCarInsRegn(e.target.value)}
+          />    
+        </div>
+        <div className='row'>
+          <label>Life and Health Insurance</label>
+          <input
+            type="number"
+            value={lifeHealthIns}
+            onChange={(e) => setLifeHealthIns(e.target.value)}
+          />  
+        </div>
       </form>
     </div>
   )
